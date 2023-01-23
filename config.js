4cb5526b2c0feec11e6d9d3430c1de3d7f406faa
Vašek Potoček <vasek.potocek@post.cz>
2023-01-23 15:21:57 +0100

    Reálné časy hry a velikost týmu (partial revert 5a75642, b5c0ed7)

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const teamSize = {min: 1, max: 5};
+export const teamSize = {min: 3, max: 5};
 
 export const fees = {
   base: 1500,
@@ -11,10 +11,10 @@ export const dates = {
   regClose: new Date('2023-01-15T23:59:59.999+01:00'),
   changesClose: new Date('2023-01-20T23:59:59.999+01:00'),
   tshirtClose: new Date('2023-01-08T23:59:59.999+01:00'),
-  gameOpen: new Date('2023-01-21T19:00:00.000+01:00'),
-  gameStart: new Date('2023-01-21T20:00:00.000+01:00'),
-  gameEnd: new Date('2023-01-22T12:00:00.000+01:00'),
-  gameClose: new Date('2023-01-22T23:59:59.999+01:00')
+  gameOpen: new Date('2023-01-27T20:00:00.000+01:00'),
+  gameStart: new Date('2023-01-27T20:30:00.000+01:00'),
+  gameEnd: new Date('2023-01-28T20:00:00.000+01:00'),
+  gameClose: new Date('2023-01-28T23:59:59.999+01:00')
 };
 
 export const attemptDelay = 60;
