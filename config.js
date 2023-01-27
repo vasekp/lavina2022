323550cb7e41240eabe569f77076df1e1ce12854
Vašek Potoček <vasek.potocek@post.cz>
2023-01-27 15:09:22 +0100

    Export: nuly na začátku hry

--- config.js
+++ config.js
@@ -13,6 +13,7 @@ export const dates = {
   tshirtClose: new Date('2023-01-08T23:59:59.999+01:00'),
   gameOpen: new Date('2023-01-27T20:00:00.000+01:00'),
   gameStart: new Date('2023-01-27T20:30:00.000+01:00'),
+  gameMain: new Date('2023-01-28T06:30:00.000+01:00'),
   gameEnd: new Date('2023-01-28T20:00:00.000+01:00'),
   gameClose: new Date('2023-01-28T23:59:59.999+01:00')
 };
