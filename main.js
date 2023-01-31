f30dae65bf6e69e4b599f01369a88b02da6c8fe2
Vašek Potoček <vasek.potocek@post.cz>
2023-01-31 13:28:52 +0100

    Nápovědy, postupy, řešení schované jako spoiler

--- main.js
+++ main.js
@@ -3,4 +3,34 @@ window.addEventListener('DOMContentLoaded', () => {
   document.querySelector('nav').addEventListener('click', () => {
     chkbox.checked = !chkbox.checked;
   });
+  for(const td of document.querySelectorAll('#sifry-table td:nth-child(3), #sifry-table td:nth-child(5)')) {
+    const text = td.textContent;
+    if(text === 'není' || text === 'zde')
+      continue;
+    const inp = document.createElement('input');
+    inp.type = 'checkbox';
+    inp.hidden = true;
+    inp.classList.add('spoiler');
+    const span = document.createElement('span');
+    span.dataset.text = text;
+    const label = document.createElement('label');
+    label.append(inp, span);
+    td.replaceChildren(label);
+  }
+  for(const td of document.querySelectorAll('#sifry-table td:nth-child(4)')) {
+    const text = td.textContent;
+    if(text === 'není')
+      continue;
+    const span = document.createElement('span');
+    span.dataset.text = text;
+    span.textContent = 'Ukaž';
+    span.tabIndex = 0;
+    span.classList.add('spoiler2');
+    td.replaceChildren(span);
+  }
+  document.getElementById('sifry-table').addEventListener('click', ev => {
+    const tgt = ev.target;
+    if(tgt.classList.contains('spoiler2'))
+      alert(tgt.dataset.text);
+  });
 });
