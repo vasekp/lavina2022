3af328f0426d11c436fe63e3bd272d78b6d4e081
Vašek Potoček <vasek.potocek@post.cz>
2023-01-31 10:40:15 +0100

    Odstraněno pamatování poslední záložky

--- main.js
+++ main.js
@@ -1,22 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
-  {
-    const chkbox = document.getElementById('nav-unfold');
-    document.querySelector('nav').addEventListener('click', () => {
-      chkbox.checked = !chkbox.checked;
-    });
-  }
-  document.body.addEventListener('input', ev => {
-    if(ev.target.id.substring(0, 4) === 'sel-')
-      localStorage['lastTab'] = ev.target.id.substring(4);
+  const chkbox = document.getElementById('nav-unfold');
+  document.querySelector('nav').addEventListener('click', () => {
+    chkbox.checked = !chkbox.checked;
   });
-  showTab(localStorage['lastTab']);
 });
-
-function showTab(name) {
-  const ckbox = document.getElementById(`sel-${name}`);
-  if(!ckbox)
-    return;
-  ckbox.checked = true;
-  document.getElementById('nav-unfold').checked = false;
-  localStorage['lastTab'] = name;
-}
